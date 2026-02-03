ab7f4f732ce1065e3347a1f8f11b2075ab5c84f8
Truskie <118139229+Truskie@users.noreply.github.com>
2026-02-03 13:39:31 -0600

    Initial Commit

---
+++ exercise1.js
@@ -0,0 +1,16 @@
+function calculateArea(length, width){
+    return length * width;
+}
+
+console.log(calculateArea(5, 10));
+console.log(calculateArea(7, 3));
+console.log(calculateArea(4, 4));
+
+const calculatePerimeter = function(length, width)
+{
+    return (length + width) * 2;
+}
+
+console.log(calculatePerimeter(5, 10));
+console.log(calculatePerimeter(7, 3));
+console.log(calculatePerimeter(4, 4))
